3ba6dc55e3a498cfe9db2a16685886a129676a1f
engeneet <groksing5@gmail.com>
2025-03-05 20:54:27 +0100

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,16 @@
+from main.modules.baseCLI import *
+import random
+
+class BootEngi: #do not ask me why is it called "bootengi" lol
+    def __init__(self, cli, screen):
+        self.cli = cli
+        self.sc = screen
+
+        self.on_init()
+
+    def on_init(self):
+        self.cli.clearLines()
+        self.cli.addLine("Hello World!")
+
+    def update(self):
+        self.cli.addLine(str(random.randint(0, 9)))
